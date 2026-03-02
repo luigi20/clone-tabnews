c0cbf510d0ef9c8f74ac0ab489b70ba7fa846029
Luis Antonio <luisopentec@gmail.com>
2026-03-02 17:59:30 -0300

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>É um negócio perigoso, Frodo, sair da sua porta. Você pisa na estrada, e, se não controlar seus pés, não há como saber até onde você pode ser levado</h1>
+function Home() {
+  return (
+    <h1>
+      É um negócio perigoso, Frodo, sair da sua porta. Você pisa na estrada, e,
+      se não controlar seus pés, não há como saber até onde você pode ser
+      levado. E vc gajo? Como vão os estudos?
+    </h1>
+  );
 }
 
 export default Home;
