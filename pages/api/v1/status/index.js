0c2496a3ccd0b08a5593aac2673d80375e5c2765
Luis Antonio <luisopentec@gmail.com>
2026-07-03 09:37:14 -0300

    add  file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,5 @@
-import database from "../../../../infra/database";
+import database from "infra/database";
 async function status(request, response) {
-  console.log("oiiiiii");
   const result = await database.query("SELECT 1 + 1;");
   console.log(result);
   response.status(200).json({ chave: "ok Antônio" });
