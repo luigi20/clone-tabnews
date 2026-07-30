4285e52abf1503ebca230eaa47f1a54cfed2ae96
Luis Antonio <luisopentec@gmail.com>
2026-07-30 17:48:23 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,9 +6,13 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("GET TO /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  const response_body = await response.json();
-  expect(Array.isArray(response_body)).toBe(true);
-  expect(response_body.length).toBeGreaterThan(0);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      const response_body = await response.json();
+      expect(Array.isArray(response_body)).toBe(true);
+      expect(response_body.length).toBeGreaterThan(0);
+    });
+  });
 });
