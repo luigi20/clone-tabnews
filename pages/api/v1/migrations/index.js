09e47beedc086582b783dd6e3647448f381bdf7f
Luis Antonio <luisopentec@gmail.com>
2026-07-30 22:54:54 -0300

    refactor: change phrase

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -5,7 +5,7 @@ export default async function migrations(req, res) {
   const allowed_methods = ["GET", "POST"];
   if (!allowed_methods.includes(req.method)) {
     return res.status(405).json({
-      error: `Method ${req.method} not allowed`,
+      error: `Method ${req.method} not allowed.`,
     });
   }
   let db_client;
