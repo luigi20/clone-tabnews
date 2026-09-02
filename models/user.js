0b7f734098a3c0f6f384c69e1e425e6247c749c6
Luis Antonio <luisopentec@gmail.com>
2026-09-02 14:25:23 -0300

    feat(findonebyusername): create `user.findOneByUsername` method and `/api/v1/users/[username]` edp

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/errors.js";
+import { ValidationError, NotFoundError } from "infra/errors.js";
 async function create(user_input_values) {
   await validateUniqueEmail(user_input_values.email);
   await validateUniqueUserName(user_input_values.username);
@@ -47,8 +47,30 @@ async function create(user_input_values) {
   }
 }
 
+async function findOneByUsername(username) {
+  const result = await runSelectQuery(username);
+  return result;
+
+  async function runSelectQuery(username) {
+    const result = await database.query({
+      text: `SELECT * FROM users WHERE LOWER(username) = LOWER($1) LIMIT 1`,
+      values: [username],
+    });
+    console.log(result.rowCount);
+    if (result.rowCount === 0)
+      throw new NotFoundError({
+        name: "NotFoundError",
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+        status_code: 404,
+      });
+    return result.rows[0];
+  }
+}
+
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
---
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -0,0 +1,81 @@
+import orchestrator from "tests/orchestrator.js";
+import { version as uuid_version } from "uuid";
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("GET /api/v1/users/[username]", () => {
+  describe("Anonymous user", () => {
+    test("With exact case match", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "MesmoCase",
+          email: "mesmo.case@curso.dev",
+          password: "senha123",
+        }),
+      });
+      expect(response.status).toBe(201);
+      const response2 = await fetch(
+        "http://localhost:3000/api/v1/users/MesmoCase",
+      );
+      const response_body = await response2.json();
+      expect(response_body).toEqual({
+        id: response_body.id,
+        username: "MesmoCase",
+        email: "mesmo.case@curso.dev",
+        password: "senha123",
+        created_at: response_body.created_at,
+        updated_at: response_body.updated_at,
+      });
+      expect(response2.status).toBe(200);
+    });
+
+    test("With case mismatch", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "CaseDiferente",
+          email: "Case.Diferente@curso.dev",
+          password: "senha123",
+        }),
+      });
+      expect(response.status).toBe(201);
+      const response2 = await fetch(
+        "http://localhost:3000/api/v1/users/caseDiferente",
+      );
+      const response_body = await response2.json();
+      expect(response_body).toEqual({
+        id: response_body.id,
+        username: "CaseDiferente",
+        email: "Case.Diferente@curso.dev",
+        password: "senha123",
+        created_at: response_body.created_at,
+        updated_at: response_body.updated_at,
+      });
+      expect(response2.status).toBe(200);
+    });
+
+    test("With nonexistent", async () => {
+      const response = await fetch(
+        "http://localhost:3000/api/v1/users/caseDiferente2",
+      );
+      expect(response.status).toBe(404);
+      const response_body = await response.json();
+      expect(response_body).toEqual({
+        name: "NotFoundError",
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+        status_code: 404,
+      });
+    });
+  });
+});
