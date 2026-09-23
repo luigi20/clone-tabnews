aa85f916b3c17e436db7b7e733646eb176c0d1be
Luis Antonio <luisopentec@gmail.com>
2026-09-23 11:40:35 -0300

    chore(console.log): retirada de console.log

--- models/user.js
+++ models/user.js
@@ -3,25 +3,13 @@ import password from "models/password.js";
 import { ValidationError, NotFoundError } from "infra/errors.js";
 
 async function create(user_input_values) {
-  console.log("CREATE 1 - iniciou");
-
   await validateUniqueEmail(user_input_values.email);
-  console.log("CREATE 2 - email validado");
-
   await validateUniqueUserName(user_input_values.username);
-  console.log("CREATE 3 - username validado");
-
   await hash_password_in_object(user_input_values);
-  console.log("CREATE 4 - senha hasheada");
-
   const new_user = await runInsertQuery(user_input_values);
-  console.log("CREATE 5 - usuário inserido");
-
   return new_user;
 
   async function runInsertQuery(user_input_values) {
-    console.log("INSERT 1 - iniciando");
-
     try {
       const result = await database.query({
         text: `
@@ -36,50 +24,31 @@ async function create(user_input_values) {
         ],
       });
 
-      console.log("INSERT 2 - terminou");
-      console.log("INSERT 3 - rowCount:", result.rowCount);
-
       return result.rows[0];
     } catch (error) {
-      console.error("INSERT ERROR - erro original:", error);
-
       throw error;
     }
   }
 }
 
 async function hash_password_in_object(userInputValues) {
-  console.log("HASH 1 - iniciando");
-
   const hash_password = await password.hash(userInputValues.password);
-
-  console.log("HASH 2 - terminou");
-
   userInputValues.password = hash_password;
 }
 
 async function update(user_name, user_input_values) {
   const found_user = await findOneByUsername(user_name);
-
-  if ("username" in user_input_values) {
+  if ("username" in user_input_values)
     await validateUniqueUserName(user_input_values.username);
-  }
-
-  if ("email" in user_input_values) {
+  if ("email" in user_input_values)
     await validateUniqueEmail(user_input_values.email);
-  }
-
-  if ("password" in user_input_values) {
+  if ("password" in user_input_values)
     await hash_password_in_object(user_input_values);
-  }
-
   const user_with_new_values = {
     ...found_user,
     ...user_input_values,
   };
-
   const update_user = await runUpdateQuery(user_with_new_values);
-
   return update_user;
 
   async function runUpdateQuery(user_with_new_values) {
@@ -101,7 +70,6 @@ async function update(user_name, user_input_values) {
         user_with_new_values.password,
       ],
     });
-
     if (result.rowCount === 0) {
       throw new NotFoundError({
         name: "NotFoundError",
@@ -110,14 +78,12 @@ async function update(user_name, user_input_values) {
         status_code: 404,
       });
     }
-
     return result.rows[0];
   }
 }
 
 async function findOneByEmail(email) {
   const result = await runSelectQuery(email);
-
   return result;
 
   async function runSelectQuery(email) {
@@ -130,23 +96,19 @@ async function findOneByEmail(email) {
       `,
       values: [email],
     });
-
-    if (result.rowCount === 0) {
+    if (result.rowCount === 0)
       throw new NotFoundError({
         name: "NotFoundError",
         message: "O email informado não foi encontrado no sistema.",
         action: "Verifique se o email está digitado corretamente.",
         status_code: 404,
       });
-    }
-
     return result.rows[0];
   }
 }
 
 async function findOneByUsername(username) {
   const result = await runSelectQuery(username);
-
   return result;
 
   async function runSelectQuery(username) {
@@ -159,23 +121,18 @@ async function findOneByUsername(username) {
       `,
       values: [username],
     });
-
-    if (result.rowCount === 0) {
+    if (result.rowCount === 0)
       throw new NotFoundError({
         name: "NotFoundError",
         message: "O username informado não foi encontrado no sistema.",
         action: "Verifique se o username está digitado corretamente.",
         status_code: 404,
       });
-    }
-
     return result.rows[0];
   }
 }
 
 async function validateUniqueUserName(name) {
-  console.log("VALIDATE USERNAME - iniciando");
-
   const result = await database.query({
     text: `
       SELECT username
@@ -184,20 +141,14 @@ async function validateUniqueUserName(name) {
     `,
     values: [name],
   });
-
-  console.log("VALIDATE USERNAME - rowCount:", result.rowCount);
-
-  if (result.rowCount > 0) {
+  if (result.rowCount > 0)
     throw new ValidationError({
       message: "O username informado já está sendo utilizado.",
       action: "Utilize outro username para realizar está operação.",
     });
-  }
 }
 
 async function validateUniqueEmail(email) {
-  console.log("VALIDATE EMAIL - iniciando");
-
   const result = await database.query({
     text: `
       SELECT email
@@ -206,15 +157,11 @@ async function validateUniqueEmail(email) {
     `,
     values: [email],
   });
-
-  console.log("VALIDATE EMAIL - rowCount:", result.rowCount);
-
-  if (result.rowCount > 0) {
+  if (result.rowCount > 0)
     throw new ValidationError({
       message: "O email informado já está sendo utilizado.",
       action: "Utilize outro email para realizar está operação.",
     });
-  }
 }
 
 const user = {
